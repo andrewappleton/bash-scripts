7cc21938914a6afee24f0b1eb70ea76adbdcfef4
Andrew Appleton <andrewappleton@users.noreply.github.com>
2021-08-18 08:45:36 +0200

    Improved zip removal
    Added messaging and removed the iteration method to delete zip files, replaced with single line delete as the paramter is suitable for the rm command.

--- budget-lfmt.sh
+++ budget-lfmt.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
-FILESPEC=confserv
+if [[ -z "$1" ]] ; then
+    printf "No argument provided..\nPlease specify logfile prefix. e.g. 'confserv'\n"
+    exit 1
+fi
+FILESPEC=$1
 FILEDATE=$(date +"%Y%m%d_%H")
 ZIPNAME=$FILESPEC.$FILEDATE.zip
 KEEP=5
 #Compress files based on the date/time...
 #Doesn't matter if a file gets zipped twice...
 #Zip will replace the file if it already exists in the archive.
+echo "Writing zip file $ZIPNAMEi..."
 zip --exclude=*snapshot* $ZIPNAME $FILESPEC.$FILEDATE*.log
 
 #Delete older files...
@@ -16,12 +21,14 @@ LOGCOUNT=$(ls -al $FILESPEC.*.zip | wc -l)
 if [[ LOGCOUNT -gt  KEEP ]]
 then
     #Subtract 5 from the count of current zip files
-	LOGDELCOUNT=$(echo "$LOGCOUNT-$KEEP" | bc)
+        LOGDELCOUNT=$(echo "$LOGCOUNT-$KEEP" | bc)
     #List the files by filename only in one column...
     #iterate through the list of files and delete each one.
-	for n in $(ls -1 $FILESPEC.*.zip | head -$LOGDELCOUNT)
-	do
-		  echo Deleting $n...
-      rm -f $n;
-	done
+        for n in $(ls -1 $FILESPEC.*.zip | head -$LOGDELCOUNT)
+        do
+                #rm -f b;
+                echo "Deleting $n..."
+        rm -f $n;
+        done
 fi
+printf "\n\n\nDone!\n"
