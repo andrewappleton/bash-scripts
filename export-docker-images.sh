89836b26027cddf05f3b9ba361d354da8db6b18e
Andrew Appleton <andrewappleton@users.noreply.github.com>
2021-08-29 18:33:47 +0200

    Removed .tar option
    Sonatype Nexus IQ does not properly unarchive from a tar file, as it assumes that the tar file isa Docker imagee.  Changed this to archive to a .zip file instead.
    The .xz is really optional, as it is only used to allow uploading to a customer site where .zip files are rejected.

--- export-docker-images.sh
+++ export-docker-images.sh
@@ -1,26 +1,30 @@
 #!/bin/bash
 if [ "$#" -ne 1 ] ; then
-        echo "Please provide search expression for tag (e.g. 100.0.0)"
+	echo "Please provide search expression for tag (e.g. 100.0.0)"
 fi
 CONTAINERS=$(docker image ls --format "{{.Repository}}:{{.Tag}}"  | grep $1)
 for cname in $CONTAINERS
 do
-        image=$( echo $cname | cut -d\: -f1 )
-        filename=$( echo $cname | cut -d\: -f1 | rev | cut -d\/ -f1 | rev )
-        tag=$( echo $cname | cut -d\: -f2 )
-        currfilename=$filename-$tag.tar
-        echo Saving image $currfilename...
-        docker save -o $currfilename $image:$tag
-        filelist+=" "$currfilename
+	image=$( echo $cname | cut -d\: -f1 )
+	filename=$( echo $cname | cut -d\: -f1 | rev | cut -d\/ -f1 | rev )
+	tag=$( echo $cname | cut -d\: -f2 )
+	currfilename=$filename-$tag.tar
+	echo Saving image $currfilename...
+	docker save -o $currfilename $image:$tag
+	filelist+=" "$currfilename
 done
-echo Adding files to .tar...
-tar cvf cxc-$1.tar $filelist
-echo Deleting generated .tar files
-for fname in $filelist
-do
-        echo deleting $fname
-        rm -f $fname
-done
-echo XZing tar file...
-xz -v cxc-$1.tar
+#echo Adding files to .tar...
+#tar cvf cxc-$1.tar $filelist
+echo "Adding files to .zip..."
+zip cxc-$1.zip $filelist
+#echo Deleting generated .tar files
+#for fname in $filelist
+#do
+#	echo deleting $fname
+#	rm -f $fname
+#done
+#echo XZing tar file...
+#xz -v cxc-$1.tar
+echo "XZing zip file..."
+xz -v cxc-$1.zip
 echo Done...
