dfea0c29ad7a9dda64bbbd89092424bdaee97ee2
Andrew Appleton <andrewappleton@users.noreply.github.com>
2023-04-03 20:17:13 +0200

    Used variables for colors
    Added variables to help with readability of PS1 definition.

--- git_branch_bash.sh
+++ git_branch_bash.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+export CLICOLOR=1
+NORMAL="\[\033[00m\]"
+BLUE="\[\033[01;34m\]"
+YELLOW="\[\e[1;33m\]"
+GREEN="\[\e[1;32m\]"
+REVYELLOW="\[\033[43m\]"
+PS1USER=`echo $USER | tr '[:upper:]' '[:lower:]'`
+PS1HOST=`echo $HOSTNAME | tr '[:upper:]' '[:lower:]'`
 parse_git_branch() {
      git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
 }
-export PS1="\[\033[01;32m\]\u@\h:\[\033[01;34m\]\W\[\033[33m\]\$(parse_git_branch)\[\033[00m\] $ "
+export PS1="${GREEN}${PS1USER}@${PS1HOST}:${BLUE}\W${YELLOW}\$(parse_git_branch)${NORMAL} $ "
