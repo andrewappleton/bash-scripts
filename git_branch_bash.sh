cf855f2c2e57254081514fbccf51d8aa938f0416
Andrew Appleton <andrewappleton@users.noreply.github.com>
2023-04-04 13:07:41 +0200

    added parse_git_dirty function
    Indicates whether pending commits are needed in the repository

--- git_branch_bash.sh
+++ git_branch_bash.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-
-export CLICOLOR=1
 NORMAL="\[\033[00m\]"
 BLUE="\[\033[01;34m\]"
 YELLOW="\[\e[1;33m\]"
@@ -8,7 +6,10 @@ GREEN="\[\e[1;32m\]"
 REVYELLOW="\[\033[43m\]"
 PS1USER=`echo $USER | tr '[:upper:]' '[:lower:]'`
 PS1HOST=`echo $HOSTNAME | tr '[:upper:]' '[:lower:]'`
-parse_git_branch() {
-     git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/ [\1]/'
+function parse_git_dirty() {
+  [[ $(git status --porcelain 2> /dev/null) ]] && echo " *"
+}
+function parse_git_branch() {
+     git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/ (\1$(parse_git_dirty))/"
 }
 export PS1="${GREEN}${PS1USER}@${PS1HOST}:${BLUE}\W${YELLOW}\$(parse_git_branch)${NORMAL} $ "
